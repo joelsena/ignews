033d7b34df46c13f738a7e379198a5f05664fd23
Joel Sena <joel.sena229@gmail.com>
2021-10-28 16:58:50 -0300

    add global style and initial config

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { CustomHead } from '../shared/CustomHead'
+
+export default function Home() {
+    return (
+        <div>
+            <CustomHead title="Home - ig.news" />
+            <h1>Hello, world</h1>
+        </div>
+    )
+}
