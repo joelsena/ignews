5ffb6ce11afd60f931830726831bc1a213c6b94b
Joel Sena <joel.sena229@gmail.com>
2021-11-11 14:32:31 -0300

    add faunadb and signIn callback config

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,16 @@
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
+import { query as q } from 'faunadb'
+
+import { fauna } from '../../../services/fauna'
+
+// SGBDs que tem pool para APIs serveless
+// FaunaDB - HTTP
+// DynamoDB - AWS
+
+// Conexão rodando por 24h
+// PostgreSQL, MongoDB - Utiliza uma conexão que permea por um longo prazo.
+// O que para serveless functions não é bom porque precisaria refazer essa conexão toda vez, ou seja, muito custoso.
 
 export default NextAuth({
     // Configure one or more authentication providers
@@ -13,4 +24,47 @@ export default NextAuth({
         }),
         // ...add more providers here
     ],
+    // jwt: {
+    //     signingKey: process.env.SIGNING_KEY // Em produção é aconselhado a gerar um key correta com alguma lib
+    // },
+    callbacks: {
+        async signIn(user, account, profile) {
+            const { email } = user
+            // console.log(user)
+
+            try {
+                // FQL - linguagem que o fauna utiliza para realizar operações no banco de dados
+                // Lembrando que o fauna é um banco não relacional e schema free
+                fauna.query(
+                    q.If( // if
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    // Indíces são criados para gerar um nova estrutura de dados no intuito de facilitar a pesquisa no banco
+                                    q.Index('user_by_email'),
+                                    q.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'),
+                            { data: { email } }
+                        ),
+                        q.Get( // else
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(user.email)
+                            )
+                        )
+                    )
+                )
+
+                return true
+            } catch {
+                return false
+            }
+        }
+    }
 })
+
+// Callback => são funções que vão ser executada automaticamente após uma ação
